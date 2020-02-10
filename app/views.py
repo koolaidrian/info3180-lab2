37e1205a6ff32da1bb9db00e3c357d49ba128815
koolaidrian <chandap2011@gmail.com>
2020-02-09 22:13:46 -0500

    Lab 2 submission

--- app/views.py
+++ app/views.py
@@ -5,6 +5,8 @@ Werkzeug Documentation:  http://werkzeug.pocoo.org/documentation/
 This file creates your application.
 """
 
+import datetime
+
 from app import app
 from flask import render_template, request, redirect, url_for, flash
 
@@ -22,8 +24,16 @@ def home():
 @app.route('/about/')
 def about():
     """Render the website's about page."""
-    return render_template('about.html', name="Mary Jane")
+    return render_template('about.html', name="Chanderpaul Campbell")
 
+@app.route('/profile')
+def profile():
+    return render_template('profile.html',date = format_date_joined())
+
+
+def format_date_joined():
+    date_joined = datetime.date(2020, 2, 9)
+    return "Joined " + date_joined.strftime("%B, %Y")   
 
 ###
 # The functions below should be applicable to all Flask apps.
